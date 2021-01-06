6f740878d1ea1fca8e0187b2870f90c8721d564c
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2021-01-06 15:30:03 +0800

    Move files and update imports

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,10 +4,11 @@ import React, { Component } from 'react'
 // 2) Inline styling
 // 3) Radium and styled-component (e.g. styled.button`some css props here`)
 // 4) CSS modules (done by running npm run eject)
-import classes from './App.module.css'
 // import styled from 'styled-components'
-import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import classes from './App.module.css'
+import Cockpit from '../components/Cockpit/Cockpit'
+import Persons from '../components/Persons/Persons'
+// import ErrorBoundary from './ErrorBoundary'
 
 // 3) styled-component
 /* const StyledButton = styled.button`
@@ -35,7 +36,7 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
+/*   switchNameHandler = (newName) => {
     // DON'T DO THIS...
     // this.state.persons[0].name = 'Maximilian'
     this.setState({
@@ -46,6 +47,7 @@ class App extends Component {
       ]
     })
   }
+ */
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(person => {
@@ -95,23 +97,12 @@ class App extends Component {
       }
     } */
     let persons = null
-    let btnClasses  = ''
 
     if (this.state.showPersons) {
-       persons = (
-        <div>
-          {this.state.persons.map((person, index) => { // you get index param for free
-            return <ErrorBoundary key={person.id}>
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            </ErrorBoundary>
-          })}
-        </div>
-       )
+        persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
 
       // change button color to red upon showing Persons
       /* style.backgroundColor = 'red'
@@ -119,30 +110,15 @@ class App extends Component {
         backgroundColor: 'salmon',
         color: 'black'
       } */
-
-      btnClasses = classes.Red
-    }
-
-    const assignedClasses = []
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red) // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold) // classes = ['bold'] or classes = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-          className={btnClasses}
-          alt={this.state.showPersons.toString()}
-          onClick={this.togglePersonsHandler}
-        >
-          Toggle Persons
-        </button>
+        <Cockpit
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
