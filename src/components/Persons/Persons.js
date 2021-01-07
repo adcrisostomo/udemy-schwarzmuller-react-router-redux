e71decf5b1c1766598780bf0afb9287557486347
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2021-01-07 16:49:06 +0800

    Add condition to shouldComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -17,7 +17,14 @@ class Persons extends Component {
 
     shouldComponentUpdate (nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpdate')
-        return true
+
+        // shouldComponentUpdate() requires us to return a boolean value
+        // this does not deep compare 2 persons, only their memory references
+        if (nextProps.persons !== this.props.persons) {
+            return true
+        } else {
+            return false
+        }
     }
 
     getSnapshotBeforeUpdate (prevProps, prevState) {
