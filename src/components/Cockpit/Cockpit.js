f128eb1ed1e77c33dba0272370436cc4eb13fdba
aldycrisostomo <74892239+aldycrisostomo@users.noreply.github.com>
2021-01-07 18:19:53 +0800

    Add persons.length as App.js' prop

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -55,10 +55,10 @@ const Cockpit = (props) => {
         btnClass = classes.Red
     }
 
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       assignedClasses.push(classes.red) // classes = ['red']
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       assignedClasses.push(classes.bold) // classes = ['bold'] or classes = ['red', 'bold']
     }
 
